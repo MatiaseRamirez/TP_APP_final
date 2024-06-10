7b105a393a08c21e48f0a3d35ccc5b7d8b14a1c6
MatiaseRamirez <matiasramirez333@hotmail.com>
2024-06-10 20:59:03 -0300

    arreglo de update

--- app/src/main/java/mylogin/com/iu/update/PhoneUpdateFragment.kt
+++ app/src/main/java/mylogin/com/iu/update/PhoneUpdateFragment.kt
@@ -1,7 +1,11 @@
 package mylogin.com.iu.update
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -16,7 +20,7 @@ import mylogin.com.databinding.FragmentPhoneUpdateBinding
 import mylogin.com.model.Phone
 import mylogin.com.viewModel.PhoneViewModel
 
-abstract class PhoneUpdateFragment : Fragment(), MenuProvider {
+class PhoneUpdateFragment : Fragment(), MenuProvider {
 
     private lateinit var binding: FragmentPhoneUpdateBinding
     private val phoneViewModel by viewModels<PhoneViewModel>()
@@ -33,7 +37,7 @@ abstract class PhoneUpdateFragment : Fragment(), MenuProvider {
         phone = arguments?.getSerializable("phone") as Phone // Bundle
 
         binding.etPhone.setText(phone?.let { it.phone })
-
+        binding.etPhone.setText(phone!!.phone)
 
         binding.btnUpdateUser.setOnClickListener {
             validateFields(phone!!)
@@ -41,24 +45,78 @@ abstract class PhoneUpdateFragment : Fragment(), MenuProvider {
 
         val menuHost: MenuHost = requireActivity()
         menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        /*menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                TODO("Not yet implemented")
+            }
+        },viewLifecycleOwner, Lifecycle.State.RESUMED)*/
+
+
+
 
         return binding.root
     }
 
-    private fun validateFields(phone: Phone) {
-        val numero = binding.etPhone.text.toString()
+    private fun validateFields(phonee: Phone) {
+        val phone = binding.etPhone.text.toString()
 
-        if (numero.isNotEmpty()) {
+        if (phone.isNotEmpty() ) {
 
-            val numero = phone.copy(phone = numero)
+            //val user = User(id = user.id, name = name, lastName = lastName, age = age.toInt())
+            val phone = phonee.copy(phone = phone)
+            phoneViewModel.updatePhone(phone = phone)
 
-            phoneViewModel.updatePhone(phone = numero)
-
-            findNavController().navigate(R.id.action_updateFragmentPhone_to_listFragmentPhone2)
+            findNavController().navigate(R.id.action_updateFragmentPhone_to_listFragmentPhone)
 
         } else {
-            Toast.makeText(requireContext(), "Complete todos los campos!", Toast.LENGTH_SHORT)
-                .show()
+            Toast.makeText(requireContext(), "Complete todos los campos!", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.delete_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when(menuItem.itemId) {
+            R.id.menu_delete -> {
+                deletePhone()
+                true
+            }
+
+            else -> {
+                false
+            }
         }
     }
+
+    private fun deletePhone() {
+        val dialog = AlertDialog.Builder(requireContext())
+
+        dialog.setTitle("¿Desea Eliminar?")
+        dialog.setMessage("Esta seguro que desea eliminar ha ${phone!!.phone}")
+
+        dialog.setNegativeButton(getString(R.string.no_option)) { _,_ ->
+            return@setNegativeButton
+        }
+
+        dialog.setPositiveButton("Yes") { _, _ ->
+            phoneViewModel.deletePhone(phone=phone!!)
+            Toast.makeText(requireContext(), "User eliminado!", Toast.LENGTH_SHORT).show()
+
+            findNavController().navigate(R.id.action_updateFragmentPhone_to_listFragmentPhone)
+        }
+
+        dialog.create().show()
+    }
+
+
 }
+
